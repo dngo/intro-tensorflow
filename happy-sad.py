46fdbcdae653da2b0c4cf9726421ec76ef718aa8
David Ngo <davngo@gmail.com>
2019-05-14 17:49:03 -0400

    happy-sad exercise

---
+++ happy-sad.py
@@ -0,0 +1,70 @@
+import tensorflow as tf
+import os
+import zipfile
+
+
+DESIRED_ACCURACY = 0.999
+
+#!wget --no-check-certificate \
+#    "https://storage.googleapis.com/laurencemoroney-blog.appspot.com/happy-or-sad.zip" \
+#    -O "/tmp/happy-or-sad.zip"
+
+zip_ref = zipfile.ZipFile("/tmp/happy-or-sad.zip", 'r')
+zip_ref.extractall("/tmp/h-or-s")
+zip_ref.close()
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('acc') > DESIRED_ACCURACY):
+      print("\nReached 99.9% accuracy so cancelling training!")
+      self.model.stop_training = True
+callbacks = myCallback()
+
+model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 300x300 with 3 bytes color
+    # This is the first convolution
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(300, 300, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fifth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+
+from tensorflow.keras.optimizers import RMSprop
+
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['acc'])
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale=1/255)
+
+train_generator = train_datagen.flow_from_directory(
+        '/tmp/h-or-s/',  # This is the source directory for training images
+        target_size=(300, 300),  # All images will be resized to 150x150
+        batch_size=128,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+   
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=8,  
+      epochs=20,
+      verbose=1)
+
